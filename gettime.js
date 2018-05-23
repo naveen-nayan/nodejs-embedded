f3d675a96ca41ea38054fc4d00c677f92f64bedb
nayan8459 <naveen.nayan@zenatix.com>
2018-05-23 20:16:35 +0530

    first example to find time

--- gettime.js
+++ gettime.js
@@ -4,11 +4,12 @@ function getDateTime() {
 
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
-
+    console.log(hour);
     var min  = date.getMinutes();
+    console.log(min);
     min = (min < 10 ? "0" : "") + min;
 
-    console.log(hour + ":" + min) ;
+    console.log(hour + ":" + min);
 
 }
 
