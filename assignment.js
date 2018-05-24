132f4d97028ff43d4752870ffc8d445c7a2cf318
nayan8459 <naveen.nayan@zenatix.com>
2018-05-25 00:54:59 +0530

    assignment

--- assignment.js
+++ assignment.js
@@ -1,4 +1,3 @@
-
 //  method to find current time in 24 hour format
 function getTime() {
     var date = new Date();
@@ -13,25 +12,28 @@ function getTime() {
 var rpiDhtSensor = require('rpi-dht-sensor');
 var dht = new rpiDhtSensor.DHT22(18);
 
-function readTemperate() {
+function readTemperature() {
   var readout = dht.read();
-  temperature = readout.temperature.toFixed(18)
+  temperature = readout.temperature.toFixed(2)
   return temperature
-}
+};
 
 function onOffLogic(){
   start_time = "00:00"
   off_time = "17:00"
   temperature_limit = "27.00"
   current_time = getTime()
-  if (current_time > start_time && current_time < off_time)
-    current_temp = readTemperate()
-//    if (current_temp > temperature_limit)
-//      console.log(current_temp)
-//    else
-//      console.log("Temperatue is lower");
+  if (current_time > start_time && current_time < off_time){
+    current_temp = readTemperature()
+    if (current_temp > temperature_limit)
+      console.log(current_temp)
+    else
+      console.log("Temperatue is lower");
+   }
   else
     console.log("Time is out off bound");
-  setTimeout(read, 5000);
-}
+  setTimeout(onOffLogic, 5000);
+};
 
+
+onOffLogic();
